e206f5192dab6e5499e78c561fe32d92393b5a2a
mmarolin <matija.marolin11@gmail.com>
2022-02-04 18:44:53 +0100

    ss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
-
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import "./App.css";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+const SneakerPage = () => (
+  <div>
+    <h1>Sneaker page</h1>
+  </div>
+);
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+        <Route path="/sneakers" component={SneakerPage} />
+      </Switch>
     </div>
   );
 }

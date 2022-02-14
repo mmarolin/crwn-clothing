805e911fdb1cd71d3b8ffe58e23521dfaf49237c
mmarolin <matija.marolin11@gmail.com>
2022-02-14 16:59:13 +0100

    authentication

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import AuthenticationPage from "./pages/authentication/authentication.component";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={AuthenticationPage} />
       </Switch>
     </div>
   );

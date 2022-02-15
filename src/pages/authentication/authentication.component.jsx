810cc42ea75bcf5dc420145eac021042b24712c4
mmarolin <matija.marolin11@gmail.com>
2022-02-15 20:49:21 +0100

    signup-component

--- src/pages/authentication/authentication.component.jsx
+++ src/pages/authentication/authentication.component.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 import "./authentication.styles.scss";
 
 const AuthenticationPage = () => (
   <div className="authentication">
     <SignIn />
+    <SignUp />
   </div>
 );
 

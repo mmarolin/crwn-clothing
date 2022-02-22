3feef9f9214467a8dd2d59f72afa850c317cbe79
mmarolin <matija.marolin11@gmail.com>
2022-02-22 13:43:25 +0100

    adding-redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Switch, Route, useRouteMatch } from "react-router-dom";
+
 import "./App.css";
+
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
